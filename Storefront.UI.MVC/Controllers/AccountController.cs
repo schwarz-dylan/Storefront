95b4538a3e26f4154fa31dfceaf0b61d8cded9bb
Dylan Schwarz <schwarz_dylan@yahoo.com>
2021-02-09 14:54:48 -0600

    changes

--- Storefront.UI.MVC/Controllers/AccountController.cs
+++ Storefront.UI.MVC/Controllers/AccountController.cs
@@ -155,13 +155,8 @@ namespace Storefront.UI.MVC.Controllers
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
-                    await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
-                    
-                    // For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=320771
-                    // Send an email with this link
-                    // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
-                    // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
-                    // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
+
+                    UserManager.AddToRole(user.Id, "Customer"); //MVC 2 Account controller for ref
 
                     return RedirectToAction("Index", "Home");
                 }
